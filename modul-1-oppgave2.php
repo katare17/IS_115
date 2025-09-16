f2cc6ff1663547612ce833821e36513fde35ed41
unknown <katare17@uia.no>
2025-09-16 09:16:52 +0200

    Initial commit

---
+++ modul-1-oppgave2.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Modul 1</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor02">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Tilbake til moduler
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="modul-1-oppgave1.php">Oppgave 1</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="modul-1-oppgave2.php">Oppgave 2</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="modul-1-oppgave3.php">Oppgave 3</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="modul-1-oppgave4.php">Oppgave 4</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="modul-1-oppgave5.php">Oppgave 5</a>
+        </li>
+    </div>
+  </div>
+</nav>
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Katarina Kirkhus</title>
+</head>
+<body>
+  <div class="centered-content">   
+I teksten kommer det frem at display_errors er satt til 
+    <br>On lokalt og On på master value, dette betyr at dersom 
+    <br>det finnes feil i koden, så vil det komme opp feilkoder 
+    <br>i nettleseren. Document_root er satt til C:/xampp/htdocs, 
+    <br>hvilket er mappen der oppgaven min er lagret. Å kjenne 
+    <br>til bruken av phpinfo() tenker jeg er et godt utgangspunkt 
+    <br>for å bli kjent med hvordan php-filen fungerer og hva man 
+    <br>kan forvente av oppførsel i det man gjør videre, også 
+    <br>kjent som konfigurasjonsinnstillingene. 
+    </div>
+
+</body>
+
+<?php phpinfo();
+?>
---
+++ modul-2-oppgave4.php
@@ -0,0 +1,71 @@
+<?php 
+
+$tall1 = 19;
+$tall2= 30;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Modul 2</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor02">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Tilbake til moduler
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="modul-2-oppgave1.php">Oppgave 1</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="modul-2-oppgave2.php">Oppgave 2</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="modul-2-oppgave3.php">Oppgave 3</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="modul-2-oppgave4.php">Oppgave 4</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="modul-2-oppgave5.php">Oppgave 5</a>
+        </li>
+    </div>
+  </div>
+</nav>
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Katarina Kirkhus</title>
+</head>
+
+<body>
+    <div class="centered-content"> 
+    <h1>Oppgave 4</h1>
+    <br>
+    <p>Dette er tallene jeg har skrevet inn: <?php echo $tall1?> & <?php echo $tall2?>
+    <br><br>
+    Se hva som skjer når vi utfører dette mattestykket: <?php echo $tall1?> - <?php echo $tall2?> = <?php echo ($tall1 - $tall2)?>
+    <br>
+    Svaret blir -11. 
+    
+    <br>
+    <br>
+    Men det er jo ikke den absolutte differansen. Den er nemlig:
+    <?php
+     echo $absoluteDifference = abs($tall1 - $tall2);
+    ?>
+    <br>
+    <br> 
+    Og motsatt vei: 
+    <?php
+     echo $absoluteDifference2 = abs($tall2 - $tall1);
+    ?>
+</p>
+</div>
+
+</body>
+</html>
